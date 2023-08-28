dc9e9d7fa497898959381b7b1c11a3286bbc7f42
Jason McPeak <jason.mcpeak@gmail.com>
2023-08-28 13:19:04 -0700

    num 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing - BILL! 1:03 - &nbsp;
+          Get started by editing - Chris! 1:18 - &nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
